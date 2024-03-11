17abf856e25dcfc8ba054a88be69b1378ef3042a
asharaf007 <718romanempire@gmail.com>
2024-03-11 20:17:49 +0530

    .env updated

--- 13mega_blog/src/pages/Home.jsx
+++ 13mega_blog/src/pages/Home.jsx
@@ -1,31 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { Container, PostCard } from "../components/index";
 import { postService } from "../appwrite/index";
-import { useNavigate,useSelector } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 function Home() {
     const [posts, setPosts] = useState([]);
     const navigate = useNavigate();
-    const authStatus=useSelector(state=>state.auth.status)
+    const authStatus = useSelector((state) => state.auth.status);
     useEffect(() => {
-        if(!authStatus) return
+        if (!authStatus) return;
         postService.getPosts().then((allPosts) => {
-            if (allPosts) setPosts(allPosts.documents)
+            if (allPosts) setPosts(allPosts.documents);
             else navigate("/");
-        })
+        });
     }, []);
-    return (posts.length==0)?(
+    return posts.length == 0 ? (
         <div className="w-full py-8 mt-4 text-center">
             <Container>
                 <div className="flex flex-wrap">
                     <div className="p-2 w-full">
                         <h1 className="text-2xl font-bold hover:text-gray-500">
-                            There is no post to show
+                            {authStatus?"There is no post to show":"Login to see posts"}
                         </h1>
                     </div>
                 </div>
             </Container>
         </div>
-    ):(
+    ) : (
         <div className="w-full py-8">
             <Container>
                 <div className="flex flex-wrap rounded-xl">
